60677f0d8890b1768beef364d9ff2ca386e78f4c
dyanez11 <diegoayanezvega@hotmail.com>
2020-11-11 23:52:19 -0600

    feat: add post and put method

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const config = require('./config');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(cors());
-debugger
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+debugger;
 
 // Routes
 app.use(require('./routes/products'));
 app.use(require('./routes/login'));
 
-
 app.listen(config.port, () => {
-  console.log(`Running server on port ${config.port}`)
-})
+  console.log(`Running server on port ${config.port}`);
+});
--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const products = require('../data/products');
 
+/**
+ * Get methods
+ */
+
 router.get('/', (req, res) => {
   res.send('Hello from routes');
 });
@@ -13,6 +17,44 @@ router.get('/products', (req, res) => {
 router.get('/products/:id', (req, res) => {
   const { id } = req.params;
   res.send(products[id - 1]);
+});
+
+/**
+ * Post methods
+ */
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('received');
+});
+
+router.post('/products', (req, res) => {
+  const { name, price, img, description } = req.body;
+  const id = products.length + 1;
+  const newProduct = {
+    id,
+    name,
+    price,
+    img,
+    description,
+  };
+  products.push(newProduct);
+  res.send(products);
+});
+
+/**
+ * Put methods
+ */
+
+router.put('/products/:id', (req, res) => {
+  const { name, price, img, description } = req.body;
+  const { id } = req.params;
+  const product = products[id - 1];
+  name ? (product.name = name) : product.name;
+  price ? (product.price = price) : product.price;
+  img ? (product.img = img) : product.img;
+  description ? (product.description = description) : product.description;
+  res.send(product);
 });
 
 module.exports = router;
